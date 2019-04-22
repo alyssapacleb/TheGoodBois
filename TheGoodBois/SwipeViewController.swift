22a6f724cdbea7eb004f3b30e176a8f15884cecb
Argandona Vite, Angel R <ara2798@lib-pclem053.local>
2019-04-21 21:36:50 -0500

    Added preferences and swipe view controller

---
+++ TheGoodBois/SwipeViewController.swift
@@ -0,0 +1,72 @@
+//
+//  SwipeViewController.swift
+//  TheGoodBois
+//
+//  Created by Argandona Vite, Angel R on 4/21/19.
+//  Copyright © 2019 The Good Bois. All rights reserved.
+//
+
+import UIKit
+
+class SwipeViewController: UIViewController {
+
+    @IBOutlet weak var swipeImageView: UIImageView!
+    let imageNames = ["dog1","dog2","dog3"]
+    var currentImage = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        var swipeLeft = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+    }
+
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.Left:
+                if currentImage == imageNames.count - 1 {
+                    currentImage = 0
+                    
+                }else{
+                    currentImage += 1
+                }
+                swipeImageView.image = UIImage(named: imageNames[currentImage])
+                
+            case UISwipeGestureRecognizerDirection.Right:
+                if currentImage == 0 {
+                    currentImage = imageNames.count - 1
+                }else{
+                    currentImage -= 1
+                }
+                swipeImageView.image = UIImage(named: imageNames[currentImage])
+            default:
+                break
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
