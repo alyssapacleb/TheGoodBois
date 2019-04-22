4d4ddc1bf6d6a5495a41494baeedc951bfdb7dd0
alyssapacleb <alyssapacleb@gmail.com>
2019-04-22 13:48:21 -0500

    more constraint gore

--- TheGoodBois/ViewController.swift
+++ TheGoodBois/ViewController.swift
@@ -7,6 +7,19 @@
 //
 
 import UIKit
+/*extension NSMutableAttributedString {
+    public func setAsLink(textToFind:String, linkURL:String) -> Bool {
+        
+        let foundRange = self.mutableString.range(of: textToFind)
+        if foundRange.location != NSNotFound {
+            
+            self.addAttribute(.link, value: linkURL, range: foundRange)
+            
+            return true
+        }
+        return false
+    }
+}*/
 
 class ViewController: UIViewController {
 
