070621ff2f31dd182c8c6817f74cb6b47d9833c1
Argandona Vite, Angel R <ara2798@lib-pclem032.lib.utexas.edu>
2019-05-06 00:51:12 -0500

    Modified About Page.

---
+++ TheGoodBois/Slide.swift
@@ -0,0 +1,21 @@
+//
+//  Slide.swift
+//  TheGoodBois
+//
+//  Created by Argandona Vite, Angel R on 5/5/19.
+//  Copyright © 2019 The Good Bois. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TheGoodBois/Tutorial1ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  Tutorial1ViewController.swift
+//  TheGoodBois
+//
+//  Created by Argandona Vite, Angel R on 5/5/19.
+//  Copyright © 2019 The Good Bois. All rights reserved.
+//
+
+import UIKit
+
+class Tutorial1ViewController: UIViewController {
+
+    @IBOutlet weak var dogImage: UIImageView!
+    @IBOutlet weak var cursorImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startAnimation()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func startAnimation(){
+        UIView.animate(withDuration: 1.0, delay: 0.0, options:[.repeat], animations: {
+            self.cursorImage.center.x = self.cursorImage.center.x+300
+            self.dogImage.center.x = self.dogImage.center.x+375
+            
+        }, completion: nil)
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
